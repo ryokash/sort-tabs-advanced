2a9a32bc06361713889c068f7e9b7600cd61d782
Samuel Stark <dev+samuel.winslow.stark@gmail.com>
2022-03-21 15:51:51 +0000

    Unified formatting using Prettier

--- src/background.js
+++ src/background.js
@@ -2,79 +2,65 @@
  * Comparison functions
  */
 function compareByUrlAsc(a, b) {
-	let url1 = new URL(a.url);
-	let url2 = new URL(b.url);
+  let url1 = new URL(a.url);
+  let url2 = new URL(b.url);
 
-	return url1.hostname
-		.localeCompare(url2.hostname);
+  return url1.hostname.localeCompare(url2.hostname);
 }
 
 function compareByUrlDesc(a, b) {
-	let url1 = new URL(a.url);
-	let url2 = new URL(b.url);
+  let url1 = new URL(a.url);
+  let url2 = new URL(b.url);
 
-	return url2.hostname
-		.localeCompare(url1.hostname);
+  return url2.hostname.localeCompare(url1.hostname);
 }
 
 function compareByDomainAsc(a, b) {
-	let url1 = new URL(a.url);
-	let url2 = new URL(b.url);
+  let url1 = new URL(a.url);
+  let url2 = new URL(b.url);
 
-	let domain1 = url1.hostname
-		  .split(".")
-		  .slice(-2)
-		  .join(".");
-	let domain2 = url2.hostname
-		  .split(".")
-		  .slice(-2)
-		  .join(".");
+  let domain1 = url1.hostname.split(".").slice(-2).join(".");
+  let domain2 = url2.hostname.split(".").slice(-2).join(".");
 
-	return domain1.localeCompare(domain2);
+  return domain1.localeCompare(domain2);
 }
 
 function compareByDomainDesc(a, b) {
-	let url1 = new URL(a.url);
-	let url2 = new URL(b.url);
+  let url1 = new URL(a.url);
+  let url2 = new URL(b.url);
 
-	let domain1 = url1.hostname
-		  .split(".")
-		  .slice(-2)
-		  .join(".");
-	let domain2 = url2.hostname
-		  .split(".")
-		  .slice(-2)
-		  .join(".");
+  let domain1 = url1.hostname.split(".").slice(-2).join(".");
+  let domain2 = url2.hostname.split(".").slice(-2).join(".");
 
-	return domain2.localeCompare(domain1);
+  return domain2.localeCompare(domain1);
 }
 
 function compareByTitleAsc(a, b) {
-	return a.title.localeCompare(b.title);
+  return a.title.localeCompare(b.title);
 }
 
 function compareByTitleDesc(a, b) {
-	return b.title.localeCompare(a.title);
+  return b.title.localeCompare(a.title);
 }
 
 function compareByLastAccessAsc(a, b) {
-	if (a.lastAccessed < b.lastAccessed) {
-		return -1;
-	} else if (a.lastAccessed > b.lastAccessed) {
-		return 1;
-	} else {
-		return 0;
-	}
+  if (a.lastAccessed < b.lastAccessed) {
+    return -1;
+  } else if (a.lastAccessed > b.lastAccessed) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 function compareByLastAccessDesc(a, b) {
-	if (b.lastAccessed < a.lastAccessed) {
-		return -1;
-	} else if (b.lastAccessed > a.lastAccessed) {
-		return 1;
-	} else {
-		return 0;
-	}
+  if (b.lastAccessed < a.lastAccessed) {
+    return -1;
+  } else if (b.lastAccessed > a.lastAccessed) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 function onSettingsSortAuto(newValue) {
@@ -94,19 +80,19 @@ function onSettingsSortPinned(newValue) {
 }
 
 let menuIdToComparator = {
-	"sort-by-url-asc" : compareByUrlAsc,
-	"sort-by-url-desc" : compareByUrlDesc,
-	"sort-by-domain-asc" : compareByDomainAsc,
-	"sort-by-domain-desc" : compareByDomainDesc,
-	"sort-by-last-access-asc" : compareByLastAccessAsc,
-	"sort-by-last-access-desc" : compareByLastAccessDesc,
-	"sort-by-title-asc" : compareByTitleAsc,
-	"sort-by-title-desc" : compareByTitleDesc,
+  "sort-by-url-asc": compareByUrlAsc,
+  "sort-by-url-desc": compareByUrlDesc,
+  "sort-by-domain-asc": compareByDomainAsc,
+  "sort-by-domain-desc": compareByDomainDesc,
+  "sort-by-last-access-asc": compareByLastAccessAsc,
+  "sort-by-last-access-desc": compareByLastAccessDesc,
+  "sort-by-title-asc": compareByTitleAsc,
+  "sort-by-title-desc": compareByTitleDesc,
 };
 
 let settingsMenuIdToHandler = {
   "settings-sort-auto": onSettingsSortAuto,
-  "settings-sort-pinned": onSettingsSortPinned
+  "settings-sort-pinned": onSettingsSortPinned,
 };
 
 function sortTabsComparatorName(compName, settings) {
@@ -114,73 +100,77 @@ function sortTabsComparatorName(compName, settings) {
 }
 
 function settingsSortAutoHandler(tabId, changeInfo, tabInfo) {
-  browser.storage.local.get({
-    "last-comparator": undefined,
-    "settings-sort-auto": false,
-    "settings-sort-pinned": false
-  }).then(
-    (settings) => {
+  browser.storage.local
+    .get({
+      "last-comparator": undefined,
+      "settings-sort-auto": false,
+      "settings-sort-pinned": false,
+    })
+    .then((settings) => {
       if (menuIdToComparator[settings["last-comparator"]] !== undefined) {
-        return sortTabs(menuIdToComparator[settings["last-comparator"]], settings);
+        return sortTabs(
+          menuIdToComparator[settings["last-comparator"]],
+          settings
+        );
       }
     }, onError);
 }
 
 function sortTabs(comparator, settings) {
-	return browser.tabs.query({
-		currentWindow : true
-	}).then((tabs) => {
-		const pinnedTabs = [];
-		const normalTabs = [];
-		for (const tab of tabs) {
-			if (tab.pinned)
-				pinnedTabs.push(tab);
-			else
-				normalTabs.push(tab);
-		}
-		if (settings["settings-sort-pinned"]) {
-			sortTabsInternal(pinnedTabs, comparator);
-		}
-		sortTabsInternal(normalTabs, comparator);
-	});
+  return browser.tabs
+    .query({
+      currentWindow: true,
+    })
+    .then((tabs) => {
+      const pinnedTabs = [];
+      const normalTabs = [];
+      for (const tab of tabs) {
+        if (tab.pinned) pinnedTabs.push(tab);
+        else normalTabs.push(tab);
+      }
+      if (settings["settings-sort-pinned"]) {
+        sortTabsInternal(pinnedTabs, comparator);
+      }
+      sortTabsInternal(normalTabs, comparator);
+    });
 }
 
 function sortTabsInternal(tabs, comparator) {
-	if (tabs.length == 0)
-		return;
+  if (tabs.length == 0) return;
 
-	const offset = tabs[0].index;
-	const beforeIds = tabs.map(tab => tab.id);
-	const afterIds = tabs.slice(0).sort(comparator).map(tab => tab.id);
-	let currentIds = beforeIds.slice(0);
-	for (const difference of differ.diff(beforeIds, afterIds)) {
-		if (!difference.added)
-			continue;
-		const movingIds = difference.value;
-		const lastMovingId = movingIds[movingIds.length - 1];
-		const nearestFollowingIndex = afterIds.indexOf(lastMovingId) + 1;
-		let newIndex = nearestFollowingIndex < afterIds.length ? currentIds.indexOf(afterIds[nearestFollowingIndex]) : -1;
-		if (newIndex < 0)
-			newIndex = beforeIds.length;
-		const oldIndex = currentIds.indexOf(movingIds[0]);
-		if (oldIndex < newIndex)
-			newIndex--;
-		browser.tabs.move(movingIds, {
-			index: newIndex + offset
-		});
-		currentIds = currentIds.filter(id => !movingIds.includes(id));
-		currentIds.splice(newIndex, 0, ...movingIds);
-	}
+  const offset = tabs[0].index;
+  const beforeIds = tabs.map((tab) => tab.id);
+  const afterIds = tabs
+    .slice(0)
+    .sort(comparator)
+    .map((tab) => tab.id);
+  let currentIds = beforeIds.slice(0);
+  for (const difference of differ.diff(beforeIds, afterIds)) {
+    if (!difference.added) continue;
+    const movingIds = difference.value;
+    const lastMovingId = movingIds[movingIds.length - 1];
+    const nearestFollowingIndex = afterIds.indexOf(lastMovingId) + 1;
+    let newIndex =
+      nearestFollowingIndex < afterIds.length
+        ? currentIds.indexOf(afterIds[nearestFollowingIndex])
+        : -1;
+    if (newIndex < 0) newIndex = beforeIds.length;
+    const oldIndex = currentIds.indexOf(movingIds[0]);
+    if (oldIndex < newIndex) newIndex--;
+    browser.tabs.move(movingIds, {
+      index: newIndex + offset,
+    });
+    currentIds = currentIds.filter((id) => !movingIds.includes(id));
+    currentIds.splice(newIndex, 0, ...movingIds);
+  }
 }
 
 function settingChanged(settingId, newValue) {
-  return settingsMenuIdToHandler[settingId](newValue)
-    .then(
-      (e) => {
-        return browser.storage.local.set({
-          [settingId]: newValue
-        });
-      }, onError);
+  return settingsMenuIdToHandler[settingId](newValue).then((e) => {
+    return browser.storage.local.set({
+      [settingId]: newValue,
+    });
+  }, onError);
 }
 
 function onError(error) {
--- src/popup/sortabs.js
+++ src/popup/sortabs.js
@@ -5,74 +5,85 @@
 /**
  * menu definitions
  */
-let menuDefs = [{
-	id : "sort-by-url-asc",
-	title : "sort by url (asc)",
-	contexts : ["tools_menu", "browser_action"],
-	icons : {
-		16 : "icons/sort-icon-url-asc-16.png"
-	}
-}, {
-	id : "sort-by-url-desc",
-	title : "sort by url (desc)",
-	contexts : ["tools_menu", "browser_action"],
-	icons : {
-		16 : "icons/sort-icon-url-desc-16.png"
-	}
-}, {
-	id : "sort-by-domain-asc",
-	title : "sort by domain (asc)",
-	contexts : ["tools_menu", "browser_action"],
-	icons : {
-		16 : "icons/sort-icon-domain-asc-16.png"
-	}
-}, {
-	id : "sort-by-domain-desc",
-	title : "sort by domain (desc)",
-	contexts : ["tools_menu", "browser_action"],
-	icons : {
-		16 : "icons/sort-icon-domain-desc-16.png"
-	}
-}, {
-	id : "sort-by-title-asc",
-	title : "sort by title (asc)",
-	contexts : ["tools_menu", "browser_action"],
-	icons : {
-		16 : "icons/sort-icon-title-asc-16.png"
-	}
-}, {
-	id : "sort-by-title-desc",
-	title : "sort by title (desc)",
-	contexts : ["tools_menu", "browser_action"],
-	icons : {
-		16 : "icons/sort-icon-title-desc-16.png"
-	}
-}, {
-	id : "sort-by-last-access-asc",
-	title : "sort by last access (asc)",
-	contexts : ["tools_menu", "browser_action"],
-	icons : {
-		16 : "icons/sort-icon-access-time-asc-16.png"
-	}
-}, {
-	id : "sort-by-last-access-desc",
-	title : "sort by last access (desc)",
-	contexts : ["tools_menu", "browser_action"],
-	icons : {
-		16 : "icons/sort-icon-access-time-desc-16.png"
-	}
-}];
+let menuDefs = [
+  {
+    id: "sort-by-url-asc",
+    title: "sort by url (asc)",
+    contexts: ["tools_menu", "browser_action"],
+    icons: {
+      16: "icons/sort-icon-url-asc-16.png",
+    },
+  },
+  {
+    id: "sort-by-url-desc",
+    title: "sort by url (desc)",
+    contexts: ["tools_menu", "browser_action"],
+    icons: {
+      16: "icons/sort-icon-url-desc-16.png",
+    },
+  },
+  {
+    id: "sort-by-domain-asc",
+    title: "sort by domain (asc)",
+    contexts: ["tools_menu", "browser_action"],
+    icons: {
+      16: "icons/sort-icon-domain-asc-16.png",
+    },
+  },
+  {
+    id: "sort-by-domain-desc",
+    title: "sort by domain (desc)",
+    contexts: ["tools_menu", "browser_action"],
+    icons: {
+      16: "icons/sort-icon-domain-desc-16.png",
+    },
+  },
+  {
+    id: "sort-by-title-asc",
+    title: "sort by title (asc)",
+    contexts: ["tools_menu", "browser_action"],
+    icons: {
+      16: "icons/sort-icon-title-asc-16.png",
+    },
+  },
+  {
+    id: "sort-by-title-desc",
+    title: "sort by title (desc)",
+    contexts: ["tools_menu", "browser_action"],
+    icons: {
+      16: "icons/sort-icon-title-desc-16.png",
+    },
+  },
+  {
+    id: "sort-by-last-access-asc",
+    title: "sort by last access (asc)",
+    contexts: ["tools_menu", "browser_action"],
+    icons: {
+      16: "icons/sort-icon-access-time-asc-16.png",
+    },
+  },
+  {
+    id: "sort-by-last-access-desc",
+    title: "sort by last access (desc)",
+    contexts: ["tools_menu", "browser_action"],
+    icons: {
+      16: "icons/sort-icon-access-time-desc-16.png",
+    },
+  },
+];
 
-let settingsDefs = [{
-	id : "settings-sort-auto",
-	title : "sort automatically",
-	contexts : ["tools_menu", "browser_action"]
-}, {
-  id: "settings-sort-pinned",
-  title: "sort pinned tabs",
-	contexts : ["tools_menu", "browser_action"]
-}];
-
+let settingsDefs = [
+  {
+    id: "settings-sort-auto",
+    title: "sort automatically",
+    contexts: ["tools_menu", "browser_action"],
+  },
+  {
+    id: "settings-sort-pinned",
+    title: "sort pinned tabs",
+    contexts: ["tools_menu", "browser_action"],
+  },
+];
 
 function onError(error) {
   console.trace(error);
@@ -80,77 +91,74 @@ function onError(error) {
 
 function initializeSettings() {
   let defaultDict = settingsDefs.reduce(
-    (acc, cur, idx, src) => Object.assign(acc, {[cur.id]: false}),
-    {});
+    (acc, cur, idx, src) => Object.assign(acc, { [cur.id]: false }),
+    {}
+  );
   return browser.storage.local.get(defaultDict);
 }
 
 function clickHandler(evt, settings) {
   let backgroundWindow = browser.runtime.getBackgroundPage();
-  backgroundWindow.then(
-    (w) => w.sortTabsComparatorName(evt.target.id, settings))
-		.then(
-      (tab) => {
-        console.log("Click handler: " + evt.target.id);
-        return browser.storage.local.set({
-          "last-comparator": evt.target.id
-        }).then(
-				  () => window.close(),
-          onError);
-			}, onError);
+  backgroundWindow
+    .then((w) => w.sortTabsComparatorName(evt.target.id, settings))
+    .then((tab) => {
+      console.log("Click handler: " + evt.target.id);
+      return browser.storage.local
+        .set({
+          "last-comparator": evt.target.id,
+        })
+        .then(() => window.close(), onError);
+    }, onError);
 }
 
 function settingsClickHandler(evt, settings) {
   let backgroundWindow = browser.runtime.getBackgroundPage();
-  return backgroundWindow.then(
-    (w) => w.settingChanged(evt.target.id, evt.target.checked));
+  return backgroundWindow.then((w) =>
+    w.settingChanged(evt.target.id, evt.target.checked)
+  );
 }
 
 function createButton(buttonDef, settings) {
-	let newEl = document.createElement('div');
-	newEl.id = buttonDef.id;
+  let newEl = document.createElement("div");
+  newEl.id = buttonDef.id;
   newEl.innerText = buttonDef.title;
-	// newEl.src = "../" + buttonDef.icons[16];
-	newEl.addEventListener(
-    "click",
-    (evt) => clickHandler(evt, settings));
-	return newEl;
+  // newEl.src = "../" + buttonDef.icons[16];
+  newEl.addEventListener("click", (evt) => clickHandler(evt, settings));
+  return newEl;
 }
 
 function createSettingsToggle(buttonDef, settings) {
-  let newEl = document.createElement('div');
+  let newEl = document.createElement("div");
   newEl.id = buttonDef.id;
-  let checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
   checkbox.id = buttonDef.id;
   checkbox.name = buttonDef.id;
-  let label = document.createElement('label');
+  let label = document.createElement("label");
   label.innerText = buttonDef.title;
   label.htmlFor = buttonDef.id;
   checkbox.checked = settings[buttonDef.id];
 
   newEl.appendChild(checkbox);
   newEl.appendChild(label);
-  newEl.addEventListener(
-    "click",
-    (evt) => settingsClickHandler(evt, settings));
+  newEl.addEventListener("click", (evt) => settingsClickHandler(evt, settings));
   return newEl;
 }
 
 function createPopup(settings) {
   console.log(settings);
   const settingsGroup = document.createElement("div");
-  const settingsButtons = settingsDefs.map(
-    (def) => createSettingsToggle(def, settings));
+  const settingsButtons = settingsDefs.map((def) =>
+    createSettingsToggle(def, settings)
+  );
   settingsButtons.forEach((button) => settingsGroup.appendChild(button));
 
-  const buttons = menuDefs.map(
-    (menuDef) => createButton(menuDef, settings));
+  const buttons = menuDefs.map((menuDef) => createButton(menuDef, settings));
   const buttonGroup = document.createElement("div");
   buttons.forEach((button) => buttonGroup.appendChild(button));
 
-	let cont = document.getElementById("options");
-	cont.appendChild(buttonGroup);
+  let cont = document.getElementById("options");
+  cont.appendChild(buttonGroup);
   cont.appendChild(document.createElement("hr"));
   let settingsCont = document.getElementById("settings");
   settingsCont.appendChild(settingsGroup);
@@ -159,11 +167,8 @@ function createPopup(settings) {
 /**
  * init
  */
-document.addEventListener(
-  "DOMContentLoaded",
-  (evt) => {
-    initializeSettings().then(
-      (settings) => {
-        createPopup(settings);
-      }, onError);
-  });
+document.addEventListener("DOMContentLoaded", (evt) => {
+  initializeSettings().then((settings) => {
+    createPopup(settings);
+  }, onError);
+});
